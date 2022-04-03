fff1b14f5fdd404e237a7a3020e92ed106fb266a
AdarshGrewal <adarshgrewal@gmail.com>
2022-04-03 19:18:28 +0530

    onyx: Include MiuiCamera
    Change-Id: I03fe0f13775e74301ca0d3aaca81f0509a5a8cbb

--- device.mk
+++ device.mk
@@ -492,3 +492,6 @@ PRODUCT_COPY_FILES += \
 
 # Vendor
 $(call inherit-product, vendor/xiaomi/onyx/onyx-vendor.mk)
+
+# Vendor MiuiCamera
+$(call inherit-product-if-exists, device/xiaomi/onyx-miuicamera/device.mk)
