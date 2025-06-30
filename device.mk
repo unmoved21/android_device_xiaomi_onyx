f159009695c37b320e85010efb9489b151905751
Arian <arian.kulmer@web.de>
2025-06-30 11:13:55 +0530

    onyx: Move to double tap sensor
    Change-Id: Ib4803011fefad35c8b7a2d1e2b7d2744b32b13f4

--- device.mk
+++ device.mk
@@ -352,8 +352,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
-$(call soong_config_set,power_libperfmgr,mode_extension_lib,//$(LOCAL_PATH):libpowermode-ext-onyx)
-
 # Properties
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/properties/odm_CN.prop:$(TARGET_COPY_OUT_ODM)/etc/odm_CN.prop \
