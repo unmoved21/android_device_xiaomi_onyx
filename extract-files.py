8a862bdc73b6396710c3ad83ba97d10e966c7216
Bruno Martins <bgcngm@gmail.com>
2026-01-25 16:05:20 +0000

    onyx: Build all possible soundfx modules from sources
    Change-Id: I1548d3a5c2dc8176914c457beac8b80b12168340

--- extract-files.py
+++ extract-files.py
@@ -321,7 +321,6 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('android.media.audio.common.types-V4-ndk.so', 'android.media.audio.common.types-V3-ndk.so')
         .replace_needed('android.hardware.audio.effect-V2-ndk.so', 'android.hardware.audio.effect-V3-ndk.so'),
     (
-        'vendor/lib64/soundfx/libbundleaidl.so',
         'vendor/lib64/libaudioserviceexampleimpl.so',
     ): blob_fixup()
         .replace_needed('libaudio_aidl_conversion_common_ndk.so', 'libaudio_aidl_conversion_common_ndk_prebuilt.so'),
