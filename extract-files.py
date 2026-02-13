cc26f4e692ff48f7e554cf914a3c353e395cbe30
unmoved21 <unmoved2121@gmail.com>
2026-02-13 14:41:43 +0000

    onyx: Update WFD system blobs from dada OS3.0.6.0.WOCMIXM
    Change-Id: I669970b5faa7c94c48aa56defbd9988969c59f79

--- extract-files.py
+++ extract-files.py
@@ -66,12 +66,6 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
     'system_ext/etc/vintf/manifest/vendor.qti.qesdsys.service.xml': blob_fixup()
         .regex_replace(r'(?s)^.*?(?=<manifest)', ''),
-    'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
-        .add_needed('libgui_shim.so'),
-    'system_ext/lib64/libwfdnative.so': blob_fixup()
-        .add_needed('libbinder_shim.so')
-        .add_needed('libinput_shim.so')
-        .remove_needed('android.hidl.base@1.0.so'),
     'vendor/etc/clstc_config_library.xml': blob_fixup()
         .regex_replace(r'<library>\s*<name>libdolbyclstc[\s\S]*?</library>', ''),
     'vendor/etc/sensors/hals.conf': blob_fixup()
