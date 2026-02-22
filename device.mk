d69ced617ee958c1a1a43f1bf105f4224dc88373
unmoved21 <unmoved2121@gmail.com>
2026-02-22 18:58:01 +0000

    onyx: Bring back dolby audio
    Change-Id: If5a135f484c85a109d6828530ac5ed4f315ae282

--- device.mk
+++ device.mk
@@ -74,7 +74,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.core-V2-ndk.vendor \
     android.hardware.audio.core.sounddose-V1-ndk.vendor \
     libalsautilsv2.vendor \
-    libaudioaidlcommon.vendor \
     libaudioutils_shim \
     libmediautils_vendor.vendor \
     libmemunreachable.vendor
@@ -82,7 +81,7 @@ PRODUCT_PACKAGES += \
 AUDIO_HAL_DIR := hardware/qcom-caf/sm8750/audio/primary-hal
 
 PRODUCT_COPY_FILES += \
-    $(AUDIO_HAL_DIR)/configs/sun/audio_effects_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_tuna/audio_effects_config.xml \
+    $(LOCAL_PATH)/configs/audio/audio_effects_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_tuna/audio_effects_config.xml \
     $(LOCAL_PATH)/configs/audio/audio_module_config_primary.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/audio_module_config_primary.xml \
     $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_tuna/audio_policy_configuration.xml
 
@@ -145,6 +144,10 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
+
+# Dolby
+PRODUCT_PACKAGES += \
+    LunarisDolby
 
 # Fastbootd
 PRODUCT_PACKAGES += \
@@ -258,9 +261,6 @@ $(call soong_config_set_bool,lineage_health,charging_control_supports_bypass,fal
 
 # Media
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media/media_codecs_tuna_v0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_tuna_v0.xml \
-    $(LOCAL_PATH)/configs/media/media_codecs_performance_tuna_v0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_tuna_v0.xml \
-    $(LOCAL_PATH)/configs/media/video_system_specs.json:$(TARGET_COPY_OUT_VENDOR)/etc/media_tuna_v0/video_system_specs.json \
     $(LOCAL_PATH)/configs/media/media_profiles_tuna_v0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_tuna_v0.xml
 
 PRODUCT_COPY_FILES += \
--- extract-files.py
+++ extract-files.py
@@ -66,8 +66,6 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
     'system_ext/etc/vintf/manifest/vendor.qti.qesdsys.service.xml': blob_fixup()
         .regex_replace(r'(?s)^.*?(?=<manifest)', ''),
-    'vendor/etc/clstc_config_library.xml': blob_fixup()
-        .regex_replace(r'<library>\s*<name>libdolbyclstc[\s\S]*?</library>', ''),
     'vendor/etc/sensors/hals.conf': blob_fixup()
         .regex_replace('.*vl53l8.*\n?', ''),
     'vendor/lib64/libaudioserviceexampleimpl.so': blob_fixup()
@@ -170,6 +168,11 @@ blob_fixups: blob_fixups_user_type = {
     ): blob_fixup()
         .replace_needed('android.hardware.sensors-V2-ndk.so', 'android.hardware.sensors-V3-ndk.so'),
     (
+        'vendor/etc/media_codecs_tuna_v0.xml',
+        'vendor/etc/media_codecs_tuna_v0_without_dvenc.xml',
+    ): blob_fixup()
+        .regex_replace('.*media_codecs_(google_audio|google_c2|google_telephony|google_video|vendor_audio).*\n', ''),
+    (
         'odm/lib64/libAncHumanPreviewBokeh.so',
         'odm/lib64/libMiEmojiEffect.so',
         'odm/lib64/libMiVideoFilter.so',
@@ -190,22 +193,11 @@ blob_fixups: blob_fixups_user_type = {
     ): blob_fixup()
         .replace_needed('libultrahdr.so', 'libultrahdr_prebuilt.so'),
     (
-        'vendor/lib64/hw/android.hardware.bluetooth.audio_sw.so',
-        'vendor/lib64/hw/libaudioeffecthal.qti.so',
-        'vendor/lib64/libaudio_aidl_conversion_common_ndk_prebuilt.so',
-        'vendor/lib64/libaudioserviceexampleimpl.so',
-        'vendor/lib64/soundfx/libbundleaidl.so',
-        'vendor/lib64/soundfx/libdownmixaidl.so',
-        'vendor/lib64/soundfx/libdynamicsprocessingaidl.so',
-        'vendor/lib64/soundfx/libloudnessenhanceraidl.so',
-        'vendor/lib64/soundfx/libqcompostprocbundle.so',
-        'vendor/lib64/soundfx/libqcomvisualizer.so',
-        'vendor/lib64/soundfx/libqcomvoiceprocessing.so',
-        'vendor/lib64/soundfx/libreverbaidl.so',
-        'vendor/lib64/soundfx/libvisualizeraidl.so',
-        'vendor/lib64/soundfx/libvolumelistener.so'
-    ): blob_fixup()
-        .replace_needed('android.media.audio.common.types-V6-ndk.so', 'android.media.audio.common.types-V3-ndk.so'),
+          'vendor/lib64/soundfx/liblvacfsprocessingaidl.so',
+          'vendor/lib64/soundfx/libdlbvolaidl.so',
+     ): blob_fixup()
+        .replace_needed('android.media.audio.common.types-V4-ndk.so', 'android.media.audio.common.types-V3-ndk.so')
+        .replace_needed('android.hardware.audio.effect-V2-ndk.so', 'android.hardware.audio.effect-V3-ndk.so'),
     (
         'vendor/lib64/soundfx/libbundleaidl.so',
         'vendor/lib64/libaudioserviceexampleimpl.so',
