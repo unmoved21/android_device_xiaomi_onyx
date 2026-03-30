0d461ab523eba4cf633bf074ed62307efb865b16
AdarshGrewal <adarshgrewal@gmail.com>
2026-03-30 13:52:39 +0530

    fixup! onyx: Switch to dummy micharge service
    Change-Id: If8b4531f87271d13d1733b85d3c102531a95e42a

--- device.mk
+++ device.mk
@@ -184,10 +184,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     xiaomi.system.hypsys.common-service-dummy
 
-# Micharge
-PRODUCT_PACKAGES += \
-    vendor.xiaomi.hardware.micharge-service-dummy
-
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health-service.qti \
@@ -275,6 +271,10 @@ PRODUCT_COPY_FILES += \
 # Memtrack
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.memtrack-service
+
+# Micharge
+PRODUCT_PACKAGES += \
+    vendor.xiaomi.hardware.micharge-service-dummy
 
 # Network
 PRODUCT_COPY_FILES += \
