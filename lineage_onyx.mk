56e8d6d33ca514874b63657b420609627db750d1
UnmoveD <unmoved2121@gmail.com>
2026-05-02 20:30:25 +0000

    onyx: Update from OS3.0.7.0.WOLMIXM
    Change-Id: I377d55b8d08c17f68b7d9920a5c0a5fbf404765b

--- lineage_onyx.mk
+++ lineage_onyx.mk
@@ -23,8 +23,8 @@ PRODUCT_SYSTEM_NAME := onyx_global
 PRODUCT_SYSTEM_DEVICE := onyx
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="onyx_global-user 16 BP2A.250605.031.A3 OS3.0.6.0.WOLMIXM release-keys" \
-    BuildFingerprint=POCO/onyx_global/onyx:16/BP2A.250605.031.A3/OS3.0.6.0.WOLMIXM:user/release-keys \
+    BuildDesc="onyx_global-user 16 BP2A.250605.031.A3 OS3.0.7.0.WOLMIXM release-keys" \
+    BuildFingerprint=POCO/onyx_global/onyx:16/BP2A.250605.031.A3/OS3.0.7.0.WOLMIXM:user/release-keys \
     DeviceName=$(PRODUCT_SYSTEM_DEVICE) \
     DeviceProduct=$(PRODUCT_SYSTEM_NAME)
 
