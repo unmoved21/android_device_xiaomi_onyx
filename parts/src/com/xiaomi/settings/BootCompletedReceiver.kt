8320950f0393f87ff062fe1f89c0f1ba16aa5f08
unmoved <unmoved@ServerHive>
2026-07-12 18:37:06 +0000

    Revert "onyx: parts: Properly handle min refresh rate"
    This reverts commit b4b3a89917aa4127689784f6ef3ba9433c035bac.

--- parts/src/com/xiaomi/settings/BootCompletedReceiver.kt
+++ parts/src/com/xiaomi/settings/BootCompletedReceiver.kt
@@ -14,11 +14,11 @@ import android.os.UserHandle
 import android.util.Log
 import android.view.Display
 import android.view.Display.HdrCapabilities
-import com.xiaomi.settings.display.RefreshRateService
 import com.xiaomi.settings.display.ColorService
 
 /** Everything begins at boot. */
 class BootCompletedReceiver : BroadcastReceiver() {
+    private val KEY_MIN_REFRESH_RATE = "min_refresh_rate"
 
     companion object {
         private const val TAG = "BootReceiver"
@@ -37,8 +37,6 @@ class BootCompletedReceiver : BroadcastReceiver() {
     }
 
     private fun onLockedBootCompleted(context: Context) {
-        // Display
-        context.startServiceAsUser(Intent(context, RefreshRateService::class.java), UserHandle.CURRENT)
 
         // Display
         ColorService.startService(context)
@@ -51,5 +49,17 @@ class BootCompletedReceiver : BroadcastReceiver() {
             HdrCapabilities.HDR_TYPE_HLG,
             HdrCapabilities.HDR_TYPE_HDR10_PLUS
         ))
+
+        // Set minimum refresh rate to 60Hz
+        try {
+            android.provider.Settings.System.putFloat(
+                context.contentResolver, 
+                KEY_MIN_REFRESH_RATE, 
+                60f
+            )
+            if (DEBUG) Log.d(TAG, "Set $KEY_MIN_REFRESH_RATE to 60Hz")
+        } catch (e: Exception) {
+            Log.e(TAG, "Failed to set $KEY_MIN_REFRESH_RATE", e)
+        }
     }
 }
