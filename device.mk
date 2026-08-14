4f0e115bb675335f9460b5253299bf0600eb5195
AdarshGrewal <adarshgrewal@gmail.com>
2026-08-14 15:02:14 +0530

    onyx: Blob out more audio libs
    Change-Id: I344dbd6e22bdf529fdd2c1da02bb4241abf10419

--- device.mk
+++ device.mk
@@ -58,7 +58,6 @@ PRODUCT_PACKAGES += \
     libagm_compress_plugin \
     libagm_mixer_plugin \
     libagm_pcm_plugin \
-    libagmipcservice \
     libaudiochargerlistener \
     libbatterylistener \
     libcustomva_intf \
