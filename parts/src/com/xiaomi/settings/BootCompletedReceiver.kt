9a04aa154cafdca2f5122ee64376003852c9a2bf
unmoved21 <unmoved2121@gmail.com>
2026-09-10 22:12:19 +0000

    Reapply "onyx: parts: Update overrideHdrTypes API for U"
    This reverts commit a0f0956553e6b8be84eeabedeed086b7e16e2f89.
    
    Change-Id: Ia11f2cbff59984ec789336465c985b38fb4b1135

--- parts/src/com/xiaomi/settings/BootCompletedReceiver.kt
+++ parts/src/com/xiaomi/settings/BootCompletedReceiver.kt
@@ -9,9 +9,10 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.os.IBinder;
+import android.hardware.display.DisplayManager;
 import android.util.Log
+import android.view.Display;
 import android.view.Display.HdrCapabilities;
-import android.view.SurfaceControl;
 import com.xiaomi.settings.display.ColorService
 
 class BootCompletedReceiver : BroadcastReceiver() {
@@ -24,9 +25,9 @@ class BootCompletedReceiver : BroadcastReceiver() {
         }
 
         // Override HDR types
-        val displayToken = SurfaceControl.getInternalDisplayToken()
-        SurfaceControl.overrideHdrTypes(
-            displayToken,
+        val displayManager = context.getSystemService(DisplayManager::class.java)
+        displayManager.overrideHdrTypes(
+            Display.DEFAULT_DISPLAY,
             intArrayOf(
                 HdrCapabilities.HDR_TYPE_DOLBY_VISION,
                 HdrCapabilities.HDR_TYPE_HDR10,
